4ea9a5aa5eb2f7bd0f4bf60bfa401e3865282b9a
jai111 <bajpaijai51@gmail.com>
2021-08-18 22:25:42 +0530

    Update LoginPage.js

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,115 +1,62 @@
-mport React, { useState } from "react";
-import { withRouter } from "react-router-dom";
-import { loginUser } from "../../../_actions/user_actions";
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import { Form, Icon, Input, Button, Checkbox, Typography } from 'antd';
-import { useDispatch } from "react-redux";
-
-const { Title } = Typography;
-
+import React, { useState } from 'react'
+import Axios from 'axios'
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
+import { withRouter } from 'react-router-dom';
 function LoginPage(props) {
-  const dispatch = useDispatch();
-  
-  const [formErrorMessage, setFormErrorMessage] = useState('')
-  
-  
+    const dispatch = useDispatch();
 
-  return (
-    <Formik
-      initialValues={{
-        email: '',
-        password: '',
-      }}
-      validationSchema={Yup.object().shape({
-        email: Yup.string()
-          .email('Email is invalid')
-          .required('Email is required'),
-        password: Yup.string()
-          .min(6, 'Password must be at least 6 characters')
-          .required('Password is required'),
-      })}
-      onSubmit={(values) => {
-        setTimeout(() => {
-          let dataToSubmit = {
-            email: values.email,
-            password: values.password
-          };
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
 
-          dispatch(loginUser(dataToSubmit))
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(body))
             .then(response => {
-              if (response.payload.loginSuccess) {
-                if (rememberMe === true) {
-                  window.localStorage.setItem('rememberMe', values.id);
+                if (response.payload.loginSuccess) {
+                    props.history.push('/')
                 } else {
-                  localStorage.removeItem('rememberMe');
+                    alert('Error˝')
                 }
-                props.history.push("/");
-              } else {
-                setFormErrorMessage('Check out your Account or Password again')
-              }
             })
-            .catch(err => {
-              setFormErrorMessage('Check out your Account or Password again')
-              setTimeout(() => {
-                setFormErrorMessage("")
-              }, 3000);
-            });
-         
-        }, 500);
-      }}
-    >
-      {props => {
-        
-        return (
-          <div className="app">
 
-            <Title level={2}>Log In</Title>
-            <form onSubmit={handleSubmit} style={{ width: '350px' }}>
 
-              <Form.Item required>
-                <Input
-                  id="email"
-                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="Enter your email"
-                  type="email"
-                  value={values.email}
-                  
-                />
-                
-              </Form.Item>
+    }
 
-              <Form.Item required>
-                <Input
-                  id="password"
-                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="Enter your password"
-                  type="password"
-                 
-                />
-                
-              </Form.Item>
 
-              {formErrorMessage && (
-                <label ><p style={{ color: '#ff0000bf', fontSize: '0.7rem', border: '1px solid', padding: '1rem', borderRadius: '10px' }}>{formErrorMessage}</p></label>
-              )}
-
-              <Form.Item>
-                
-               
-                <div>
-                  <Button type="primary" htmlType="submit" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
-                    Log in
-                </Button>
-                </div>
-                Or <a href="/register">register now!</a>
-              </Form.Item>
+    return (
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+            <form style={{ display: 'flex', flexDirection: 'column' }}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                <br />
+                <button type="submit">
+                    Login
+                </button>
             </form>
-          </div>
-        );
-      }}
-    </Formik>
-  );
-};
+        </div>
+    )
+}
 
-export default withRouter(LoginPage);
+export default withRouter(LoginPage)
