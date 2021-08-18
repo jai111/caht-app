b0bc99e4e5ab9ee6af6a7d67e92c0f79d08def73
jai111 <bajpaijai51@gmail.com>
2021-08-18 22:30:09 +0530

    Update RegisterPage.js

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -32,7 +32,7 @@ function RegisterPage(props) {
         event.preventDefault();
 
         if (Password !== ConfirmPassword) {
-            return alert('비밀번호와 비밀번호 확인은 같아야 합니다.')
+            return alert('')
         }
 
         let body = {
