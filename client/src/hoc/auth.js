76783351271b23ae5e81f03680834166a42011bf
jai111 <bajpaijai51@gmail.com>
2021-08-19 11:54:41 +0530

    Update auth.js

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,39 +1,38 @@
 import React, { useEffect } from 'react';
-import { auth } from '../_actions/user_actions';
-import { useSelector, useDispatch } from "react-redux";
+import Axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { auth } from '../_actions/user_action';
 
-export default function (ComposedClass, reload, adminRoute = null) {
+export default function (SpecificComponent, option, adminRoute = null) {
+
+    
     function AuthenticationCheck(props) {
-
-        let user = useSelector(state => state.user);
         const dispatch = useDispatch();
 
         useEffect(() => {
 
-            dispatch(auth()).then(async response => {
-                if (await !response.payload.isAuth) {
-                    if (reload) {
-                        props.history.push('/register_login')
+            dispatch(auth()).then(response => {
+                console.log(response)
+               
+                if (!response.payload.isAuth) {
+                    if (option) {
+                        props.history.push('/login')
                     }
                 } else {
+                  
                     if (adminRoute && !response.payload.isAdmin) {
                         props.history.push('/')
-                    }
-                    else {
-                        if (reload === false) {
+                    } else {
+                        if (option === false)
                             props.history.push('/')
-                        }
                     }
                 }
             })
-            
-        }, [dispatch, props.history, user.googleAuth])
+        }, [])
 
         return (
-            <ComposedClass {...props} user={user} />
+            <SpecificComponent />
         )
     }
     return AuthenticationCheck
 }
-
-
